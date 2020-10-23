022865dae64e44b69dbcf2b340a628464e0fff4d
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2020-10-23 14:48:45 +0300

    aloitusrakenne

---
+++ public/js/pankkitiedot.js
@@ -0,0 +1,7 @@
+function laheta_pankkitiedot() {
+  var nimi = document.getElementById("name").value;
+  var pin = document.getElementById("pin").value;
+
+  console.log(nimi);
+  console.log(pin);
+}
---
+++ server.js
@@ -0,0 +1,6 @@
+console.log("Moikka moi!")
+
+const express = require('express')
+const app = express()
+app.listen(3000, () => console.log("kuuntelen"));
+app.use(express.static("public"));
