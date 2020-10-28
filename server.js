1c45ea367224e4f79a3694b6ad533a0452b222bd
vaksis <54946934+asecoder3@users.noreply.github.com>
2020-10-28 15:21:05 +0200

    second update

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ console.log("Moikka moi!")
 
 const express = require('express')
 const app = express()
-app.listen(3000, () => console.log("kuuntelen"));
+app.listen(3000, () => console.log("kuuntelen http://localhost:3000"));
 app.use(express.static("public"));
