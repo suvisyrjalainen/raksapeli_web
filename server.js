0c4e46c5078c6aaefefc3a9bba7c321ebeda3c87
vaksis <62132561+vaksis@users.noreply.github.com>
2020-10-29 11:57:53 +0200

    Delete server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-console.log("Moikka moi!")
-
-const express = require('express')
-const app = express()
-app.listen(3000, () => console.log("kuuntelen http://localhost:3000"));
-app.use(express.static("public"));
