76134a49f4ac75bf4438f80daa1746ef3f847cc7
vaksis YT <veetixx8@gmail.com>
2020-11-18 10:26:22 +0200

    added content to about us

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -9,8 +9,29 @@ class About extends Component {
 
     render() {
         return (
-            <div>
-                <h2>About scene</h2>
+            <div class="wrapper">
+                <div class="col-half">
+                    <h2>About scene</h2>
+                    <p>industry. Lorem Ipsum has been the industry's standard dummy text ever Lorem Ipsum.</p>
+                </div>
+
+                <div class="col-half">
+                    <h3>Lorem Ipsum is simply dummy text of the printing and typesetting</h3>
+                    <p>industry. Lorem Ipsum has been the industry's standard dummy text ever Lorem Ipsum.</p>
+                    <p>text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                </div>
+                
+                <div class="col-third">
+                    <p>industry. Lorem Ipsum has been the industry's standard dummy text ever Lorem Ipsum.</p>
+                </div>
+                <div class="col-third">
+                    <p>industry. Lorem Ipsum has been the industry's standard dummy text ever Lorem Ipsum.</p>
+                    <p>text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                </div>
+                <div class="col-third">
+                    <p>industry. Lorem Ipsum has been the industry's standard dummy text ever Lorem Ipsum.</p>
+                    <p>text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                </div>
             </div>
         );
     }
