03dc6f1c3ae64c0ba71bf78382423413f44a36b1
vaksis <54946934+asecoder3@users.noreply.github.com>
2020-11-27 10:41:56 +0200

    build and templateData is now in components folder

--- my-app/src/components/Game.js
+++ my-app/src/components/Game.js
@@ -1,20 +1,55 @@
 import React, { Component } from 'react';
-import { Link, Redirect, useLocation } from "react-router-dom";
+import Unity, { UnityContext } from "react-unity-webgl";
+
+const unityContext = new UnityContext({
+    buildUrl = "Build",
+    loaderUrl = buildUrl + "/Builds.loader.js",
+    config = {
+        dataUrl: buildUrl + "/Builds.data.gz",
+        frameworkUrl: buildUrl + "/Builds.framework.js.gz",
+        codeUrl: buildUrl + "/Builds.wasm.gz",
+        streamingAssetsUrl: "StreamingAssets",
+        companyName: "DefaultCompany",
+        productName: "Raksaopetuspeli",
+        productVersion: "0.1",
+      };
+
+    container = document.querySelector("#unity-container"),
+    canvas = document.querySelector("#unity-canvas"),
+    loadingBar = document.querySelector("#unity-loading-bar"),
+    progressBarFull = document.querySelector("#unity-progress-bar-full"),
+    fullscreenButton = document.querySelector("#unity-fullscreen-button"),
+
+      if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+        container.className = "unity-mobile";
+        config.devicePixelRatio = 1;
+      } else {
+        canvas.style.width = "960px";
+        canvas.style.height = "600px";
+      }
+      loadingBar.style.display = "block";
+
+      script = document.createElement("script");
+      script.src = loaderUrl;
+      script.onload = () => {
+        createUnityInstance(canvas, config, (progress) => {
+          progressBarFull.style.width = 100 * progress + "%";
+        }).then((unityInstance) => {
+          loadingBar.style.display = "none";
+          fullscreenButton.onclick = () => {
+            unityInstance.SetFullscreen(1);
+          };
+        }).catch((message) => {
+          alert(message);
+        });
+      };
+      document.body.appendChild(script);
+});
 
 class Game extends Component {
-    constructor(props) {
-        super(props);
-    };
-
-
     render() {
-        return (
-            <div class="game">
-                <h2>Game scene</h2>
-                <div class="column">
-                    <p>gaming is fun</p>
-                </div>
-            </div>
+        return(
+            <Unity unityContext={unityContext} />
         );
     }
 }
