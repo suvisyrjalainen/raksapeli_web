26985852174706e719e9f0bfcf1ad1d0d7dad233
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2020-11-27 12:31:55 +0200

    react unity muutoksia

--- my-app/src/components/Game.js
+++ my-app/src/components/Game.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import { Link, Redirect, useLocation } from "react-router-dom";
+import Unity, { UnityContext } from "react-unity-webgl";
 
 class Game extends Component {
     constructor(props) {
         super(props);
-    };
 
+        // Next up create a new Unity Context object to
+        // initialise and define your WebGL build. The
+        // paths are relative from your index file.
+
+        this.unityContext = new UnityContext({
+          loaderUrl: "unity/Build/Builds.loader.js",
+          dataUrl: "unity/Build/Builds.data",
+          frameworkUrl: "unity/Build/Builds.framework.js",
+          codeUrl: "unity/Build/Builds.wasm"
+        });
+      }
 
     render() {
         return (
@@ -13,6 +24,7 @@ class Game extends Component {
                 <h2>Game scene</h2>
                 <div class="column">
                     <p>gaming is fun</p>
+                    <Unity unityContext={this.unityContext} />;
                 </div>
             </div>
         );
